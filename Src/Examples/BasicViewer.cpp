d7bc796c5a413cd27ffa797acd6c1794db68b052
Zen Shawn <xiaozisheng2008@qq.com>
2022-03-17 14:09:00 +0800

    add gizmo

--- Src/Examples/BasicViewer.cpp
+++ Src/Examples/BasicViewer.cpp
@@ -1,11 +1,66 @@
 #include "DemoWindow.h"
 #include <QApplication>
 
+// int main(int argc, char **argv)
+// {
+//     QApplication app(argc, argv);
+//     DemoWindow* win = new DemoWindow;
+//     win->show();
+//     app.exec();
+//     return 0;
+// }
+
+#include <osg/ShapeDrawable>
+#include <osg/StateSet>
+#include <osg/Material>
+#include <osgManipulator/RotateCylinderDragger>
+#include <osgDB/ReadFile>
+#include <GizmoDrawable.h>
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
-    DemoWindow* win = new DemoWindow;
-    win->show();
-    app.exec();
-    return 0;
+    osg::setNotifyLevel(osg::FATAL);
+    osg::ref_ptr<osg::MatrixTransform> scene = new osg::MatrixTransform;
+    osg::Cylinder *cylinder =
+        new osg::Cylinder(osg::Vec3(0.f, 0.f, 0.f), 0.25f, 0.5f);
+    osg::ShapeDrawable *sd = new osg::ShapeDrawable(cylinder);
+    sd->setColor(osg::Vec4(0.8f, 0.5f, 0.2f, 1.f));
+    osg::StateSet *stateSet = sd->getOrCreateStateSet();
+    osg::Material *material = new osg::Material;
+    material->setColorMode(osg::Material::AMBIENT_AND_DIFFUSE);
+    stateSet->setAttributeAndModes(material);
+    stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON);
+    scene->addChild(sd);
+
+    osg::ref_ptr<GizmoDrawable> gizmo = new GizmoDrawable;
+    gizmo->setTransform(scene.get());
+    gizmo->setGizmoMode(GizmoDrawable::MOVE_GIZMO);
+
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+    geode->addDrawable(gizmo.get());
+    geode->setCullingActive(false); // allow gizmo to always display
+    geode->getOrCreateStateSet()->setRenderingHint(
+        osg::StateSet::TRANSPARENT_BIN); // always show at last
+
+    osg::ref_ptr<osg::MatrixTransform> root = new osg::MatrixTransform;
+    root->addChild(scene.get());
+    root->addChild(geode.get());
+
+    osgViewer::Viewer viewer;
+    viewer.setSceneData(root.get());
+    viewer.setCameraManipulator(new MyTrackballManipulator);
+    viewer.addEventHandler(new osgGA::StateSetManipulator(
+        viewer.getCamera()->getOrCreateStateSet()));
+    viewer.addEventHandler(new osgViewer::StatsHandler);
+    viewer.addEventHandler(new osgViewer::WindowSizeHandler);
+    viewer.realize();
+
+    osgViewer::GraphicsWindow *gw = dynamic_cast<osgViewer::GraphicsWindow *>(
+        viewer.getCamera()->getGraphicsContext());
+    if (gw) {
+        // Send window size for libGizmo to initialize
+        int x, y, w, h;
+        gw->getWindowRectangle(x, y, w, h);
+        viewer.getEventQueue()->windowResize(x, y, w, h);
+    }
+    return viewer.run();
 }
--- Src/Examples/DemoWindow.cpp
+++ Src/Examples/DemoWindow.cpp
@@ -3,25 +3,50 @@
 #include <osg/ShapeDrawable>
 #include <osg/StateSet>
 #include <osg/Material>
+#include <osgManipulator/RotateCylinderDragger>
+#include <osgDB/ReadFile>
+#include <GizmoDrawable.h>
+#include <TouchballManipulator.h>
+
 #include <QApplication>
 
-osg::Geode *createScene()
+osg::ref_ptr<osg::Node> createScene()
 {
-    // Create a simple scene with a pretty cylinder
-    osg::Geode *root = new osg::Geode;
-
+    osg::ref_ptr<osg::MatrixTransform> scene = new osg::MatrixTransform;
     osg::Cylinder *cylinder =
         new osg::Cylinder(osg::Vec3(0.f, 0.f, 0.f), 0.25f, 0.5f);
     osg::ShapeDrawable *sd = new osg::ShapeDrawable(cylinder);
     sd->setColor(osg::Vec4(0.8f, 0.5f, 0.2f, 1.f));
-    root->addDrawable(sd);
-
     osg::StateSet *stateSet = sd->getOrCreateStateSet();
     osg::Material *material = new osg::Material;
     material->setColorMode(osg::Material::AMBIENT_AND_DIFFUSE);
     stateSet->setAttributeAndModes(material);
     stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON);
+    scene->addChild(sd);
 
+    osg::ref_ptr<GizmoDrawable> gizmo = new GizmoDrawable;
+    gizmo->setTransform(scene.get());
+    gizmo->setGizmoMode(GizmoDrawable::MOVE_GIZMO);
+
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+    geode->addDrawable(gizmo.get());
+    geode->setCullingActive(false); // allow gizmo to always display
+    geode->getOrCreateStateSet()->setRenderingHint(
+        osg::StateSet::TRANSPARENT_BIN); // always show at last
+
+    osg::ref_ptr<osg::MatrixTransform> root = new osg::MatrixTransform;
+    root->addChild(scene.get());
+    root->addChild(geode.get());
+    // Create a simple scene with a pretty cylinder
+    // osg::Transform *root = new osg::Transform;
+
+    // root->addDrawable(sd);
+
+
+    // GizmoDrawable *gizmo = new GizmoDrawable();
+    // gizmo->setGizmoMode(GizmoDrawable::Mode::MOVE_GIZMO);
+    // gizmo->setDisplayScale(4);
+    // root->addDrawable(gizmo);
     return root;
 }
 
@@ -31,7 +56,7 @@ DemoWindow::DemoWindow(QWidget *parent)
     statusBar()->showMessage("Ready", 10000);
     // There is only a single widget to show - the OSG widget
     setCentralWidget(widget);
-
+    widget->getViewer()->setCameraManipulator(new TouchballManipulator());
     widget->setSceneData(createScene());
 
     // By default the rendered OSG view will only be updated when the widget
